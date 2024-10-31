5caa8bfc49aa667f2f1aadd95035491ef8d3e93b
rabaasourour <so.gomgom@gmail.com>
2024-10-31 10:31:24 +0100

    refactor the code

--- script.js
+++ script.js
@@ -1,53 +1,108 @@
-function calculateScore(callback) {
-    const correctAnswers = {
-        q1: "Paris",
-        q2: "Mercure",
-        q3: "Jupitaire",
+let currentQuestionIndex = 0
+let questions = []
+let selectedDifficulty = ""
+let score = 0
+
+async function loadQuestions(difficulty) {
+    console.log("difficulté choisie" + difficulty)
+
+    try {
+        const response = await fetch("questions.json")
+        const AllQuestions = await response.json()
+
+        questions = AllQuestions.filter((q) => q.difficulty === difficulty)
+        selectedDifficulty = difficulty
+        let currentQuestionIndex = 0
+
+        startQuiz()
+    } catch (error) {
+        console.log("Erreur lors du chargement des queqtions", error)
     }
-
-
-    const form = document.getElementById("quiz-form")
-    let score = 0
-
-    for (const question in correctAnswers) {
-        const userAnswer = form[question].value
-        if (userAnswer === correctAnswers[question]) {
-            score++
-        }
-    }
-
-    callback(score)
 }
 
-function displayReselt(score, callback) {
-    const resultDiv = document.getElementById("result")
-    resultDiv.innerHTML = 'votre score est de ${score}'
-
-    callback(score)
+function startQuiz() {
+    document.querySelector('.difficulty-btn').classList.add("hidden")
+    document.getElementById('quiz-container').classList.remove("hidden")
+    showQuestion();
 }
 
-function handleMessage(score) {
-    const resultDiv = document.getElementById("result")
-    resultDiv.classList.remove("excellent", "good", "try-again")
-    if (score === 3) {
-        resultDiv.innerHTML += "<br> Exellent !"
-        resultDiv.classList.add("exellent")
-    } else if (score === 2) {
-        resultDiv.innerHTML += "<br> bon travail !"
-        resultDiv.classList.add("good")
+function showQuestion() {
+    if (currentQuestionIndex < questions.length) {
+
+        const questionData = question[currentQuestionIndex]
+        console.log("question data" + questionData)
+        const questionContainer = document.getElementById("quiz-container")
+
+        questionContainer.innerHTML =
+            `
+        <div class = "question">
+        <p>${questionData.question}</p>
+        </div>
+
+        <form id ="quiz-form">
+        ${questionData.options
+                .map((option, index) =>
+                    `
+            <label type="radio" name="answer" value="${option}">${option}</label>
+
+                `)
+                .join("")}
+            <button type="button" onClick="submitAnswer()"> Soumettre </button>
+        `
     } else {
-        resultDiv.innerHTML += "<br> vous pouver faire mieux !"
-        resultDiv.classList.add("try-again !")
+        showFinalResult()
     }
 }
 
-function submitQuiz() {
-    calculateScore(function (score) {
-        displayReselt(score, function () {
-            handleMessage(score)
-        })
-    })
+function submitAnswer(){
+    const form = document.getElementById("quiz-form")
+    const selectedAnswer = form.answer.value
+
+    if (!selectedAnswer){
+        alert("veuillez sélectionner une réponse.")
+        return
+    }
+    checkAnswer(selectedAnswer)
 }
+
+function checkAnswer(selectedAnswer){
+    const cuerrentQuestion = questions[currentQuestionIndex]
+    if(selectedAnswer === cuerrentQuestion.answer){
+        incrementScore()
+    }
+}
+
+function incrementScore(){
+    score++
+}
+
+function showFinalResult(){
+    const quizContainer = document.getElementById("quiz-container")
+    quizContainer.innerHTML =
+
+    `
+    <div id="result">
+    <p>Votre score final est de ${score} sur ${questions.length}.</p>
+    </div>
+    `
+}
+
+document.querySelectorAll(".difficulty-btn").forEach(btn => {
+    btn.addEventListener("click", function () {
+        const level = btn.getAttribute("data-level");
+        loadQuestions(level);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const storedUsername = localStorage.getItem('username')
+    const isAuthenticated = localStorage.getItem('isAuthenticated')
+    if (storedUsername && (isAuthenticated == "true")) {
+        showUserMenu(storedUsername)
+    } else {
+        window.location.href = "login.html"
+    }
+})
 
 function checkAuth() {
     const isAuthenticated = localStorage.getItem("isAuthenticated")
@@ -64,60 +119,8 @@ function showUserMenu(username) {
 
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const storedUsername = localStorage.getItem('username')
-    const isAuthenticated = localStorage.getItem('isAuthenticated')
-    if (storedUsername && (isAuthenticated == "true")) {
-        showUserMenu(storedUsername)
-    } else {
-        window.location.href = "login.html"
-    }
-})
-
 document.getElementById("logout-btn").addEventListener("click", function () {
     localStorage.setItem("isAuthenticated", false)
     window.location.href = "login.html"
 })
 
-let currentQuestionIndex = 0
-let questions = []
-let selectedDifficulty = ""
-
-async function loadQuestions(difficulty) {
-console.log("difficulté choisie" + difficulty)
-
-    try {
-        const response = await fetch("questions.json")
-        questions = await response.json()
-
-        const FilteredQuestions = questions.filter(
-            (q) => q.difficulty === difficulty
-        )
-        selectedDifficulty = difficulty 
-        let currentQuestionIndex = 0
-
-        startQuiz()
-    } catch (error) {
-        console.log("Erreur lors du chargement des queqtions", error)
-    }
-}
-
-function startQuiz(){
-    document.querySelector('.difficulty-btn').classList.add("hidden")
-    document.getElementById('quiz-container').classList.remove("hidden")
-    showQuestion();
-}
-
-function showQuestion(){
-    if(currentQuestionIndex< questions.length){
-        const questionData = question[currentQuestionIndex]
-        console.log("question data" + questionData)
-    }
-}
-
-document.querySelectorAll(".difficulty-btn").forEach(btn => {
-    btn.addEventListener("click", function () {
-        const level = btn.getAttribute("data-level");
-        loadQuestions(level);
-    });
-});
